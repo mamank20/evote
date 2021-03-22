227d36862e24d42f343e930ed31da8708ae76ca2
Sudarman <67144921+mamank20@users.noreply.github.com>
2021-03-22 12:13:22 +0900

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/evote/';
+$config['base_url'] = 'https://mamank20.github.io/evote/;
 
 /*
 |--------------------------------------------------------------------------
